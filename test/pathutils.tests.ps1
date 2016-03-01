8e4ce738a8d3ffdadfb32363b09fd74086f114b9
jakub.pawlowski <pawlowskijakub@gmail.com>
2016-03-01 05:28:18 +0100

    fix tests

--- test/pathutils.tests.ps1
+++ test/pathutils.tests.ps1
@@ -13,14 +13,22 @@ Describe "module import test" {
 Describe "relative path feature" {
     $cases = @(
         @{ 
-            path1 = "c:\src\ps-modules\csproj\test\input\test\sln\Sample.Solution";
-            path2 = "C:\src\ps-modules\csproj\test\input\test\sln\Sample.Solution\..\..\src\Console\Console1\..\..\Core\Core.Library1\Core.Library1.csproj"
+            path1 = "test\sln\Sample.Solution";
+            path2 = "test\sln\Sample.Solution\..\..\src\Console\Console1\..\..\Core\Core.Library1\Core.Library1.csproj"
             expected = "..\..\src\Core\Core.Library1\Core.Library1.csproj" 
         }
     )
+    In "testdrive:/" {
     It "Should return relative path <expected> for <path1> and <path2>" -TestCases $cases {
         param($path1, $path2, $expected)
+        # make sure these paths exists, because only then will they will be resolved to full paths
+        if (!(test-path $path1)) { $null = new-item -type directory $path1 }
+        if (!(test-path (split-path -parent $path2))) {
+            $null = new-item -type directory (split-path -parent $path2)
+            "" | Out-File $path2 
+        }   
         $relpath = Get-RelativePath $path1 $path2
         $relpath | Should be $expected
+    }
     }
 }
